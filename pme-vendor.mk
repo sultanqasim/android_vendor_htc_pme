0feb400be64a32cc3b5ba209a79723fc7aecda65
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-05-23 13:17:11 -0400

    pme: add prebuilt packages to list of product packages
    Change-Id: Ib0c39abda7238a832bf3ef0ce8c8efda4f5b16a8

--- pme-vendor.mk
+++ pme-vendor.mk
@@ -12,4 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# Pick up overlay for features that depend on non-open-source files
+PRODUCT_PACKAGES += \
+    TimeService \
+    libril \
+    libtime_genoff \
+    libTimeService
+
 $(call inherit-product, vendor/htc/pme/pme-vendor-blobs.mk)
