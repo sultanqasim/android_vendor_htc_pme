247f32d2fc3ec7b70bf2320cc331be25ca4359be
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-05-23 13:14:31 -0400

    pme: add TimeService apk
    Change-Id: I1448e75af9e277aa356abfd804768fbff0f11de3

--- Android.mk
+++ Android.mk
@@ -16,6 +16,17 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter pme,$(TARGET_DEVICE)),)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := TimeService
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_OWNER  := htc
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmm-disp-apis
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
