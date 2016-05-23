bffa15d780a3f7dd2405ff1fb055686aac5c6842
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-05-23 10:01:46 -0400

    pme: add qcom sensor service
    Change-Id: I267af3edd2adafbf04baeb10dac49639225093ed

--- pme-vendor-blobs.mk
+++ pme-vendor-blobs.mk
@@ -24,6 +24,7 @@ PRODUCT_COPY_FILES += \
     vendor/htc/pme/proprietary/bin/qseecomd:system/bin/qseecomd \
     vendor/htc/pme/proprietary/bin/radish:system/bin/radish \
     vendor/htc/pme/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+    vendor/htc/pme/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
     vendor/htc/pme/proprietary/bin/thermal-monitor:system/bin/thermal-monitor \
     vendor/htc/pme/proprietary/bin/time_daemon:system/bin/time_daemon \
     vendor/htc/pme/proprietary/etc/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
